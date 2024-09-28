b83510ef83712dc3f5009ad60dae290b56bee997
DariaHalchenko <154507353+DariaHalchenko@users.noreply.github.com>
2024-09-28 16:06:26 +0300

    Kommentaaride lisamine klassi Ball

--- Jalgpalli mäng/Ball.cs
+++ Jalgpalli mäng/Ball.cs
@@ -8,13 +8,14 @@ namespace Jalgpalli_mäng
 {
     public class Ball
     {
-        public double X { get; private set; }
-        public double Y { get; private set; }
+        public double X { get; private set; } //Palli X asend (Положение мяча по оси X)
+        public double Y { get; private set; } //Palli Y asend (Положение мяча по оси Y)
 
-        private double _vx, _vy;
+        private double _vx, _vy; //palli kiiruse salvestamine piki X- ja Y-telge (хранения скорости мяча по осям X и Y)
 
         private Game _game;
 
+        //määrab palli algsed koordinaadid (задает начальные координаты мяча)
         public Ball(double x, double y, Game game)
         {
             _game = game;
@@ -22,23 +23,31 @@ namespace Jalgpalli_mäng
             Y = y;
         }
 
+        //määrab palli kiiruse (устанавливает скорость мяча)
         public void SetSpeed(double vx, double vy)
         {
             _vx = vx;
             _vy = vy;
         }
 
+        //palli liigutades (перемещения мяча)
         public void Move()
         {
             double newX = X + _vx;
             double newY = Y + _vy;
+            //Kontrollige, kas palli uus asukoht on staadionil
+            //Проверка, находится ли новая позиция мяча в пределах стадиона
             if (_game.Stadium.IsIn(newX, newY))
             {
+                //Kui pall on piirides, värskendab selle koordinaate
+                //Если мяч находится в пределах, обновляет его координаты
                 X = newX;
                 Y = newY;
             }
             else
             {
+                //Kui pall läheb mänguväljast välja, muutub palli kiirus 0 ja peatab selle liikumise
+                //Если мяч выходит за пределы поля, скорость мяча становится 0 и останавливает его движение
                 _vx = 0;
                 _vy = 0;
             }
